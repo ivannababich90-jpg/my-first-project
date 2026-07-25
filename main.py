a2ee2370d11e51d00a26a8fa2469c598a4a1afc4
ivann <ivanna.babich.90@gmail.com>
2026-07-25 19:25:24 +0200

    Homework

--- main.py
+++ main.py
@@ -407,8 +407,61 @@ import keyword
 #
 #print("OK")
 
+# Homework 8.1
 
+#def add_one(some_list):
+#
+#    number = int("".join(map(str, some_list)))
+#
+#    number += 1
+#
+#    return [int(i) for i in str(number)]
+#
+#assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], "Test1"
+#
+#assert add_one([9, 9, 9]) == [1, 0, 0, 0], "Test2"
+#
+#assert add_one([0]) == [1], "Test3"
+#
+#assert add_one([9]) == [1, 0], "Test4"
+#
+#print("OK")
 
+# Homework 8.2
+
+#def is_palindrome(text):
+#
+#    text = "".join(char.lower() for char in text if char.isalnum())
+#
+#    return text == text[::-1]
+#
+#assert is_palindrome("A man, a plan, a canal: Panama") == True, "Test1"
+#
+#assert is_palindrome("OP") == False, "Test2"
+#
+#assert is_palindrome("a. ") == True, "Test3"
+#
+#assert is_palindrome("aurora") == False, "Test4"
+#
+#print("OK")
+
+# Homework 8.3
+
+#def find_unique_value(some_list):
+#
+#    for number in some_list:
+#
+#        if some_list.count(number) == 1:
+#
+#            return number
+#
+#assert find_unique_value([1, 2, 1, 1]) == 2, "Test1"
+#
+#assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, "Test2"
+#
+#assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, "Test3"
+#
+#print("OK")
 
 
 
