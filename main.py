228efeea6f1b68c5125dccd6fbcd993522b9e2d0
ivann <ivanna.babich.90@gmail.com>
2026-07-31 08:59:09 +0200

    Homework

--- main.py
+++ main.py
@@ -463,5 +463,44 @@ import keyword
 #
 #print("OK")
 
+# Homework 9.1
 
+#def popular_words(text, words):
+# Переводим весь текст в нижний регистр
+#
+#    text = text.lower()
+#
+# Разделяем текст на отдельные слова
+#
+#    text_words = text.split()
+#
+# Создаем словарь с количеством каждого искомого слова
+#
+#    result = {}
+#
+#    for word in words:
+#        result[word] = text_words.count(word)
+#
+#    return result
+#
+#assert popular_words('''When I was One I had just begun When I was Two I was nearly new''',['i', 'was', 'three', 'near']) == {'i': 4,'was': 3,'three': 0,'near': 0}, 'Test1'
+#
+#print('OK')
 
+# Homework 9.2
+
+#def difference(*args):
+#    if not args:
+#        return 0
+#
+#    return round(max(args) - min(args), 2)
+#
+#assert difference(1, 2, 3) == 2, 'Test1'
+#
+#assert difference(5, -5) == 10, 'Test2'
+#
+#assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+#
+#assert difference() == 0, 'Test4'
+#
+#print('OK')
