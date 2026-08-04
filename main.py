bd0d11f5c14739c97a8c77aef017dd94e3ff0366
ivann <ivanna.babich.90@gmail.com>
2026-08-04 14:20:46 +0200

    Homework

--- main.py
+++ main.py
@@ -504,3 +504,85 @@ import keyword
 #assert difference() == 0, 'Test4'
 #
 #print('OK')
+
+# Homework 10.1
+#
+#def pow(x):
+#
+#    return x ** 2
+#
+#def some_gen(begin, end, func):
+#
+#    """
+#
+#    begin: перший елемент послідовності
+#
+#    end: кількість елементів у послідовності
+#
+#    func: функція, яка формує значення для послідовності
+#
+#    """
+#
+#    current = begin
+#
+#    for _ in range(end):
+#
+#        yield current
+#
+#        current = func(current)
+#
+#from inspect import isgenerator
+#
+#gen = some_gen(2, 4, pow)
+#
+#assert isgenerator(gen) == True, 'Test1'
+#
+#assert list(gen) == [2, 4, 16, 256], 'Test2'
+#
+#print('OK')
+
+# Homework 10.2
+#
+#def first_word(text):
+#    """Пошук першого слова"""
+#    text = text.lstrip(" .,")  # Убираем пробелы, точки и запятые в начале
+#
+#    word = ""
+#    for char in text:
+#        if char.isalpha() or char == "'":
+#            word += char
+#
+#        else:
+#            break
+#
+#    return word
+#
+#assert first_word("Hello world") == "Hello", 'Test1'
+#
+#assert first_word("greetings, friends") == "greetings", 'Test2'
+#
+#assert first_word("don't touch it") == "don't", 'Test3'
+#
+#assert first_word(".., and so on ...") == "and", 'Test4'
+#
+#assert first_word("hi") == "hi", 'Test5'
+#
+#assert first_word("Hello.World") == "Hello", 'Test6'
+#
+#print("OK")
+
+# Homework 10.3
+#
+#def is_even(digit):
+#
+#    """Перевірка чи є парним число"""
+#
+#    return digit % 2 == 0
+#
+#assert is_even(2) == True, 'Test1'
+#
+#assert is_even(5) == False, 'Test2'
+#
+#assert is_even(0) == True, 'Test3'
+#
+#print('OK')
