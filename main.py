7a1e0cf25350cddff9181daedc0849ac829b1820
ivann <ivanna.babich.90@gmail.com>
2026-08-08 16:20:17 +0200

    Homework

--- main.py
+++ main.py
@@ -586,3 +586,77 @@ import keyword
 #assert is_even(0) == True, 'Test3'
 #
 #print('OK')
+
+# Homework 12.1
+#
+# def prime_generator(end):
+#
+#     for num in range(2, end + 1):
+#
+#         is_prime = True
+#
+#         for i in range(2, int(num ** 0.5) + 1):
+#
+#             if num % i == 0:
+#
+#                 is_prime = False
+#
+#                 break
+#
+#         if is_prime:
+#
+#             yield num
+#
+# from inspect import isgenerator
+#
+# gen = prime_generator(1)
+#
+# assert isgenerator(gen) == True, 'Test0'
+#
+# assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+#
+# assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+#
+# assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+#
+# print('Ok')
+
+# Homework 12.2
+#
+# def generate_cube_numbers(end):
+#
+#     number = 2
+#
+#     while number ** 3 <= end:
+#
+#         yield number ** 3
+#
+#         number += 1
+#
+# from inspect import isgenerator
+#
+# gen = generate_cube_numbers(1)
+#
+# assert isgenerator(gen) == True, 'Test0'
+#
+# assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+#
+# assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+#
+# assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+#
+# print('Ok')
+
+# Homework 12.3
+#
+# def is_even(number):
+#
+#     return (number & 1) == 0
+#
+# assert is_even(2494563894038**2) == True, 'Test1'
+#
+# assert is_even(1056897**2) == False, 'Test2'
+#
+# assert is_even(24945638940387**3) == False, 'Test3'
+#
+# print('Ok')
